c2605d4211082676a7372490705a49cbe6ed49ac
unknown <youssef.eladawy1006@gmail.com>
2025-12-13 18:05:00 +0200

    Adjust page directories

--- Pages/account.php
+++ Pages/account.php
@@ -29,7 +29,7 @@ $userName = $_SESSION["user_name"];
     <a class="alt-logo" href="/">La Cucina Del Mare</a>
     <nav class="alt-nav">
       <ul>
-        <li><a href="/">Home</a></li>
+        <li><a href="../Pages/index.html">Home</a></li>
         <li><a href="../Pages/Menu.html">Menu</a></li>
         <li><a href="../Pages/about-alt.html">About</a></li>
         <li><a href="../Pages/contact.html">Contact</a></li>
